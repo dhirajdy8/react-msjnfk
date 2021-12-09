7a07b6e713c536baa77befbc1095da9319a8d182
dhirajdy8 <dhirajdy8@gmail.com>
2021-12-09 20:36:50 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import React, { useState, useCallback } from 'react';
-var funccount = new Set();
-const App = () => {
+import React, { Fragment, useRef, useEffect, useState } from 'react';
+function App() {
+  // Creating a ref object using useRef hook
+  const focusPoint = useRef('null');
   const [count, setCount] = useState(0);
-  const [number, setNumber] = useState(0);
-
-  const incrementCounter = useCallback(() => {
-    setCount(count + 1);
+  const onClickHandler = () => {
+    focusPoint.current.value =
+      'The quick brown fox jumps over the lazy dog' + count;
+    focusPoint.current.focus();
+    setCount((count) => count + 1);
+  };
+  useEffect(() => {
+    console.log(count);
   }, [count]);
-  const decrementCounter = useCallback(() => {
-    setCount(count - 1);
-  }, [count]);
-  const incrementNumber = useCallback(() => {
-    setNumber(number + 1);
-  }, [number]);
-
-  funccount.add(incrementCounter);
-  funccount.add(decrementCounter);
-  funccount.add(incrementNumber);
-  console.log(funccount.values);
-
   return (
-    <div>
-      Count: {count}
-      <button onClick={incrementCounter}>Increase counter</button>
-      <button onClick={decrementCounter}>Decrease Counter</button>
-      <button onClick={incrementNumber}>increase number</button>
-    </div>
+    <Fragment>
+      <div>
+        <button onClick={onClickHandler}>ACTION</button>
+      </div>
+      <label>
+        Click on the action button to focus and populate the text. {count}
+      </label>
+      <br />
+      <textarea ref={focusPoint} />
+    </Fragment>
   );
-};
+}
 
 export default App;
